6d65972f84a2062d21e8128b516f32de00a856dd
postforty <75767073+postforty@users.noreply.github.com>
2022-09-05 17:47:05 +0900

    [ADD] @RestController

--- spring-webservice/src/test/java/com/tistory/postforty/book/SpringWebserviceApplicationTests.java
+++ spring-webservice/src/test/java/com/tistory/postforty/book/SpringWebserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tistory.postforty.springwebservice;
+package com.tistory.postforty.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
