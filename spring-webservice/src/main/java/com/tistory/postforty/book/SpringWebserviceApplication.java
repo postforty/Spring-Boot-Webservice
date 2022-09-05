1f1324e4aedd0fdacb90a176b65c419c57677f62
postforty <75767073+postforty@users.noreply.github.com>
2022-09-05 17:25:12 +0900

    [UPDATE] 그룹명 변경

--- spring-webservice/src/main/java/com/tistory/postforty/book/SpringWebserviceApplication.java
+++ spring-webservice/src/main/java/com/tistory/postforty/book/SpringWebserviceApplication.java
@@ -1,4 +1,4 @@
-package com.tistory.postforty.springwebservice;
+package com.tistory.postforty.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
