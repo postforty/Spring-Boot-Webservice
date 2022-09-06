82e84b3dad81c0c449d6dbb7c353125583358321
postforty <75767073+postforty@users.noreply.github.com>
2022-09-06 11:18:29 +0900

    [ADD] Dto와 테스트 코드 추가

--- spring-webservice/src/test/java/com/tistory/postforty/book/springboot/web/HelloControllerTest.java
+++ spring-webservice/src/test/java/com/tistory/postforty/book/springboot/web/HelloControllerTest.java
@@ -1,15 +1,15 @@
 package com.tistory.postforty.book.springboot.web;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) // 테스트를 진행할 때 JUnint에 내장된 실행자 외에 다른 실행자를 실행. 스프링 부트 테스트와 JUnit 사이 연결자 역할
 @WebMvcTest(controllers = HelloController.class) // 여러 스프링 테스트 어노테이션 중 Web(Spring MVC) 기능 위주의 어노테이션
