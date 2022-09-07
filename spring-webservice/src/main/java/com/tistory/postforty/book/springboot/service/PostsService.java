0c7c430dc1a004b4a9c7f3c992bdcc09c056d124
postforty <75767073+postforty@users.noreply.github.com>
2022-09-07 17:47:06 +0900

    [UPDATE] 등록/수정/조회 API 만들기

--- spring-webservice/src/main/java/com/tistory/postforty/book/springboot/service/PostsService.java
+++ spring-webservice/src/main/java/com/tistory/postforty/book/springboot/service/PostsService.java
@@ -1,5 +1,38 @@
 package com.tistory.postforty.book.springboot.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tistory.postforty.book.springboot.domain.posts.Posts;
+import com.tistory.postforty.book.springboot.domain.posts.PostsRepository;
+import com.tistory.postforty.book.springboot.web.dto.PostsResponseDto;
+import com.tistory.postforty.book.springboot.web.dto.PostsSaveRequestDto;
+import com.tistory.postforty.book.springboot.web.dto.PostsUpdateRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor // 롬복 어노테이션. 생성자로 Bean 객체 받도록 함. Autowired와 동일한 효과. final 선언된 모든 필드를 인자값으로 하는 생성자를 생성. 해당 클래스의 의존성 관계가 변경될 때마다 생성자 코드를 계속해서 수정하는 번거로움 해결
+@Service
 public class PostsService {
+	private final PostsRepository postsRepository;
+	
+	@Transactional
+	public Long save(PostsSaveRequestDto requestDto) {
+		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+	
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+		
+		return id;
+	}
+	
+	public PostsResponseDto findById (Long id) {
+		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		
+		return new PostsResponseDto(entity);
+	}
 
 }
--- spring-webservice/src/main/java/com/tistory/postforty/book/springboot/web/PostApiController.java
+++ spring-webservice/src/main/java/com/tistory/postforty/book/springboot/web/PostApiController.java
@@ -1,5 +1,37 @@
 package com.tistory.postforty.book.springboot.web;
 
-public class PostApiController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tistory.postforty.book.springboot.service.PostsService;
+import com.tistory.postforty.book.springboot.web.dto.PostsResponseDto;
+import com.tistory.postforty.book.springboot.web.dto.PostsSaveRequestDto;
+import com.tistory.postforty.book.springboot.web.dto.PostsUpdateRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+	
+	private final PostsService postsService;
+	
+	@PostMapping("/api/v1/posts")
+	public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+		return postsService.save(requestDto);
+	}
+	
+	@PutMapping("/api/v1/posts/{id}")
+	public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+		return postsService.update(id, requestDto);
+	}
+	
+	@GetMapping("/api/v1/posts/{id}")
+	public PostsResponseDto findById (@PathVariable Long id) {
+		return postsService.findById(id);
+	}
 }
