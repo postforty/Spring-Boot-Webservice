9b57119eca809c03e8cf86bc58212609751ce070
postforty <75767073+postforty@users.noreply.github.com>
2022-09-08 15:03:10 +0900

    [ADD] Mustache - 전체 조회 화면

--- spring-webservice/src/main/java/com/tistory/postforty/book/springboot/service/PostsService.java
+++ spring-webservice/src/main/java/com/tistory/postforty/book/springboot/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.tistory.postforty.book.springboot.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tistory.postforty.book.springboot.domain.posts.Posts;
 import com.tistory.postforty.book.springboot.domain.posts.PostsRepository;
+import com.tistory.postforty.book.springboot.web.dto.PostsListResponseDto;
 import com.tistory.postforty.book.springboot.web.dto.PostsResponseDto;
 import com.tistory.postforty.book.springboot.web.dto.PostsSaveRequestDto;
 import com.tistory.postforty.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -33,6 +37,14 @@ public class PostsService {
 		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 		
 		return new PostsResponseDto(entity);
+	}
+	
+	@Transactional(readOnly = true) // 등록, 수정, 삭제 기능이 전혀 없는 서비스 메서드. 조회 기능만 남겨두어 조회 속도가 개선
+	public List<PostsListResponseDto> findAllDesc() {
+		return postsRepository.findAllDesc().stream()
+				.map(PostsListResponseDto::new)
+//				.map(posts -> new PostsListResponseDto(posts))
+				.collect(Collectors.toList());
 	}
 
 }
