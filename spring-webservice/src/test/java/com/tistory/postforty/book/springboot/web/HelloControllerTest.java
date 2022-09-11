6e43ef7bc20d419e0110ee9a90d6851ef824059b
postforty <75767073+postforty@users.noreply.github.com>
2022-09-12 00:22:51 +0900

    [ADD] 테스트 시큐리티 적용 - 스프링 시큐리티 적용으로 깨진 테스트 수정

--- spring-webservice/src/test/java/com/tistory/postforty/book/springboot/web/HelloControllerTest.java
+++ spring-webservice/src/test/java/com/tistory/postforty/book/springboot/web/HelloControllerTest.java
@@ -11,16 +11,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.tistory.postforty.book.springboot.config.auth.SecurityConfig;
+
 @RunWith(SpringRunner.class) // 테스트를 진행할 때 JUnint에 내장된 실행자 외에 다른 실행자를 실행. 스프링 부트 테스트와 JUnit 사이 연결자 역할
-@WebMvcTest(controllers = HelloController.class) // 여러 스프링 테스트 어노테이션 중 Web(Spring MVC) 기능 위주의 어노테이션
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)}) // 여러 스프링 테스트 어노테이션 중 Web(Spring MVC) 기능 위주의 어노테이션
 public class HelloControllerTest {
 	
 	@Autowired // 스프링이 관리하는 Bean 주입 받음 
 	private MockMvc mvc; // 웹 API 테스트 용으로서 HTTP GET, POST 등에 대한 API 테스트
 	
+	@WithMockUser(roles="USER")
 	@Test
 	public void hello가_리턴된다() throws Exception {
 		String hello = "hello";
@@ -30,6 +36,7 @@ public class HelloControllerTest {
 	        .andExpect(content().string(hello)); // mvc.perform의 결과 검증 - 응답 본문 내용 검증
 	}
 	
+	@WithMockUser(roles="USER")
 	@Test
 	public void helloDto가_리턴된다() throws Exception {
 		String name = "hello";
